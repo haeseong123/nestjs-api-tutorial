1e6133394b808e28dd94e33ebf65f7c0dda6c133
hs <skagotjd456@gmail.com>
2022-10-16 00:02:22 +0900

    make service : signup, signin

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+// https://www.youtube.com/watch?v=GHTA143_b-s
+// 18.19
+import { Controller, Post } from "@nestjs/common";
+import { AuthService } from "./auth.service";
+
+@Controller('auth')
+export class AuthController{
+    constructor(private authService: AuthService) {}
+
+    @Post('signup')
+    signup() {
+        return this.authService.signup()
+    }
+
+    @Post('signin')
+    signin() {
+        return this.authService.signin()
+    }
+}
