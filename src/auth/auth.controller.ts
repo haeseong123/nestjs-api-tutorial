e9c66a5c41064d30a1f54d228dd84287a3092711
hs <skagotjd456@gmail.com>
2022-10-16 23:51:59 +0900

    created User, Bookmark table using prisma

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,11 @@
 // https://www.youtube.com/watch?v=GHTA143_b-s
-// 18.19
+// 26.35
 import { Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
-export class AuthController{
-    constructor(private authService: AuthService) {}
+export class AuthController {
+    constructor(private authService: AuthService) { }
 
     @Post('signup')
     signup() {
